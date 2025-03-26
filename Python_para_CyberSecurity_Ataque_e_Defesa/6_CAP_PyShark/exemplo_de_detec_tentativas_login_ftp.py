c7616540d51f007ee6ac5b66cdeb840af7afe25b
tblackking <62818698+tblackking@users.noreply.github.com>
2025-03-25 23:50:45 -0300

    Correcao_v2_PyShark

--- Python_para_CyberSecurity_Ataque_e_Defesa/6_CAP_PyShark/exemplo_de_detec_tentativas_login_ftp.py
+++ Python_para_CyberSecurity_Ataque_e_Defesa/6_CAP_PyShark/exemplo_de_detec_tentativas_login_ftp.py
@@ -5,17 +5,18 @@ def capture_ftp_attempts(interface):
     print("[+] Iniciando monitoramento de tentativas de login FTP...")
 
     try:
-        # Captura pacotes FTP
         capture = pyshark.LiveCapture(interface=interface, display_filter='ftp')
         
-        # Exibe tentativas de login encontradas
         for packet in capture:
-            if 'USER' in packet.ftp:
-                print(f"[+] Tentativa de login FTP - Usuário: {packet.ftp.request_arg}")
-    
+            try:
+                if hasattr(packet, 'ftp') and hasattr(packet.ftp, 'request_command') and packet.ftp.request_command == 'USER':
+                    print(f"[+] Tentativa de login FTP - Usuário: {packet.ftp.request_arg}")
+            except AttributeError:
+                continue  # Pula pacotes FTP que não contêm o campo esperado
+
     except KeyboardInterrupt:
         print("[-] Captura interrompida pelo usuário.")
-    
+
     except Exception as e:
         print(f"[-] Erro durante a captura: {e}")
 
